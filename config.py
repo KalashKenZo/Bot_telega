e16045ac004514aad4f7ca3d1fae2fa655576526
KalashKenZo <83351595+KalashKenZo@users.noreply.github.com>
2022-04-03 23:34:38 +0300

    main

---
+++ config.py
@@ -0,0 +1,7 @@
+#API Token
+API_TOKEN = '5211267114:AAGzh5qVOnybsdl4DDcFN0LPdgqQ2GynGJU'
+
+#Admin ID
+admin_id = '399000894'
+
+
---
+++ main.py
@@ -0,0 +1,47 @@
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '5211267114:AAGzh5qVOnybsdl4DDcFN0LPdgqQ2GynGJU'
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("привет, это мой первый телеграмм бот️️\n")
+
+
+@dp.message_handler(regexp='(^cat[s]?$|puss)')
+async def cats(message: types.Message):
+    with open('data/cats.jpg', 'rb') as photo:
+        '''
+        # Old fashioned way:
+        await bot.send_photo(
+            message.chat.id,
+            photo,
+            caption='Cats are here 😺',
+            reply_to_message_id=message.message_id,
+        )
+        '''
+
+        await message.reply_photo(photo, caption='Cats are here 😺')
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    # old style:
+    # await bot.send_message(message.chat.id, message.text)
+
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
